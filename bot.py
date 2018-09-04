e02518db7ef881ea253b4650386c7e32afd162a1
Néstor Javier Oubiña Arriaga <nestoroa@hotmail.com>
2018-09-04 22:34:44 +0200

    Initial commit

---
+++ bot.py
@@ -0,0 +1,172 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import io
+import json
+
+import telegram
+import datetime
+import time
+import os
+
+from telegram.ext.jobqueue import Days
+
+import logger
+from logger import get_logger
+from data_loader import DataLoader
+import sys
+from telegram.ext import Updater, CommandHandler, MessageHandler, BaseFilter
+from random import normalvariate
+from telegram.error import (TelegramError, Unauthorized, BadRequest,
+                            TimedOut, ChatMigrated, NetworkError)
+from etsisi_web_scraper import news_json_scraper, events_json_scraper, avisos_json_scraper
+from upm_json_consultor import get_etsisi_degrees_info_json
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+
+def error_callback(bot, update, error):
+    try:
+        raise error
+    except Unauthorized:
+        logger.exception("remove update.message.chat_id from conversation list")
+    except BadRequest:
+        logger.exception("handle malformed requests - read more below!")
+    except TimedOut:
+        logger.exception("handle slow connection problems")
+    except NetworkError:
+        logger.exception("handle other connection problems")
+    except ChatMigrated as e:
+        logger.exception("the chat_id of a group has changed, use " + e.new_chat_id + " instead")
+    except TelegramError:
+        logger.exception("There is some error with Telegram")
+
+
+def get_schedule():
+    with io.open('horarios.json', 'r', encoding='utf8') as data_file:
+        return json.load(data_file)
+
+
+def get_chat_ids():
+    with io.open('chat_ids.json', 'r', encoding='utf8') as data_file:
+        return json.load(data_file)
+
+
+def load_settings():
+    global settings
+    global last_function_calls
+    global schedule_list
+    global chat_ids_list
+    settings = DataLoader()
+    schedule_list = get_schedule()
+    chat_ids_list = get_chat_ids()
+    last_function_calls = {}
+
+
+def is_call_available(name, chat_id, cooldown):
+    global last_function_calls
+    now = datetime.datetime.now()
+    cooldown_time = datetime.datetime.now() - datetime.timedelta(minutes=cooldown)
+    if name in last_function_calls.keys():
+        if chat_id in last_function_calls[name].keys():
+            if last_function_calls[name][chat_id] > cooldown_time:
+                last_function_calls[name][chat_id] = now
+                return False
+            else:
+                last_function_calls[name][chat_id] = now
+                return True
+        else:
+            last_function_calls[name] = {chat_id: now}
+            return True
+    else:
+        last_function_calls[name] = {chat_id: now}
+        return True
+
+
+def help(bot, update):
+    log_message(update)
+    bot.sendMessage(update.message.chat_id, settings.help_string, parse_mode=telegram.ParseMode.MARKDOWN)
+
+
+def log_message(update):
+    logger.info("He recibido: \"" + update.message.text + "\" de " + update.message.from_user.username + " [ID: " + str(
+        update.message.chat_id) + "]")
+
+
+def human_texting(string):
+    wait_time = len(string) * normalvariate(0.1, 0.05)
+    if wait_time > 8:
+        wait_time = 8
+    time.sleep(wait_time)
+
+
+def reload_data(bot, update):
+    if update.message.from_user.id == 15360527:
+        logger.info("Reloading settings")
+        load_settings()
+        bot.send_message(chat_id=update.message.chat_id, text="Datos cargados")
+
+
+def news(bot, update):
+    logger.info("Getting news")
+    text = "Estas son las últimas noticias que aparecen en la web: \n"
+    news_list = news_json_scraper()
+    for idx, new in enumerate(news_list):
+        text = text + str(idx) + ") " + news_list[new]["a-link"] + "\n"
+    bot.sendMessage(chat_id=update.message.chat.id, text=text, parse_mode=telegram.ParseMode.HTML)
+
+
+def events(bot, update):
+    logger.info("Getting news")
+    text = "Estas son los últimos eventos que aparecen en la web: \n"
+    events_list = events_json_scraper()
+    for idx, new in enumerate(events_list):
+        text = text + str(idx) + ") " + events_list[new]["a-link"] + "\n"
+    bot.sendMessage(chat_id=update.message.chat.id, text=text, parse_mode=telegram.ParseMode.HTML)
+
+
+def notifications(bot, update):
+    logger.info("Getting news")
+    text = "Estas son los últimos avisos que aparecen en la web: \n"
+    notifications_list = avisos_json_scraper()
+    for idx, new in enumerate(notifications_list):
+        text = text + str(idx) + ") " + notifications_list[new]["a-link"] + "\n"
+    bot.sendMessage(chat_id=update.message.chat.id, text=text, parse_mode=telegram.ParseMode.HTML)
+
+
+def schedule_notify(bot):
+    global chat_ids_list
+    global schedule_list
+    bot.send_message(chat_id=15360527, text="Enviando horarios")
+    for idx, chat in enumerate(chat_ids_list):
+        try:
+            text = "Va a empezar " + str(schedule_list[chat][datetime.datetime.now().hour])
+            bot.send_message(chat_id=chat_ids_list[chat]["chatid"], text=text)
+        except:
+            pass
+
+
+if __name__ == "__main__":
+    print("ETSISI Bot: Starting...")
+
+    logger = get_logger("bot_starter", True)
+    load_settings()
+
+    try:
+        logger.info("Conectando con la API de Telegram.")
+        updater = Updater(settings.telegram_token)
+        dispatcher = updater.dispatcher
+        dispatcher.add_handler(CommandHandler('help', help))
+        dispatcher.add_handler(CommandHandler('reload', reload_data))
+        dispatcher.add_handler(CommandHandler('noticias', news))
+        dispatcher.add_handler(CommandHandler('eventos', events))
+        dispatcher.add_handler(CommandHandler('avisos', notifications))
+        dispatcher.add_error_handler(error_callback)
+
+    except Exception as ex:
+        logger.exception("Error al conectar con la API de Telegram.")
+        quit()
+
+    updater.start_polling()
+    logger.info("ETSISI Bot: Estoy escuchando.")
+    updater.idle()
---
+++ etsisi_web_scraper.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import io
+import json
+from bs4 import BeautifulSoup
+import requests
+
+
+# Adquiere enlaces a las guías docentes alojadas en la web de la escuela
+def degree_json_scraper():
+    def get_degree_guides(degree_code):
+        print("Getting guides for " + str(degree_code))
+        requested_url = data['urls'][degree_code]
+        html = requests.get(requested_url)
+        soup = BeautifulSoup(html, 'html.parser')
+        asignaturas_dict = {
+        }
+        for idx, row in enumerate(soup.find_all('tr')):
+            contents = row.contents
+            codigo_asignatura = str(contents[1].string).strip("\n").strip("\t")
+            if codigo_asignatura == 'None':
+                continue
+            # Iniciamos el diccionario
+            asignaturas_dict[codigo_asignatura] = {}
+            # Guardamos nombre
+            asignaturas_dict[codigo_asignatura]["nombre"] = str(contents[3].string).strip("\n").strip("\t")
+            # Guardamos enlaces
+            links = ""
+            for link_count, link in enumerate(contents[5].findAll("a")):
+                if link_count == 0:
+                    links = "https://etsisi.upm.es" + str(link.get("href"))
+                else:
+                    links = "Enlace 1: " + links + " \n Enlace 2: https://etsisi.upm.es" + str(link.get("href"))
+                asignaturas_dict[codigo_asignatura]["guia_docente"] = links
+        return asignaturas_dict
+
+    with io.open('data.json', 'r', encoding='utf8') as data_file:
+        data = json.load(data_file)
+    degrees_guides = {}
+    for degree in data["degrees"]:
+        degrees_guides[degree] = get_degree_guides(degree)
+
+    # """We could save this in a JSON"""
+    # with io.open("data_dumped.json", 'w', encoding='utf8')  as data_file:
+    #     json.dump(degrees_guides, data_file, ensure_ascii=False)
+
+    return degrees_guides
+
+
+# Listado de noticias disponible en la web de la UPM
+def news_json_scraper():
+    html = requests.get("https://etsisi.upm.es/noticias")
+    soup = BeautifulSoup(html.text, 'html.parser')
+    news_dict = {}
+    for idx, row in enumerate(soup.find(id='main-content').findAll('a')):
+        news_dict[idx] = {}
+        news_dict[idx]["text"] = row.string
+        news_dict[idx]["a-link"] = '<a href="https://etsisi.upm.es' + row.get("href") + '">' + row.string + '</a>'
+        news_dict[idx]["link"] = 'https://etsisi.upm.es' + row.get("href")
+    return news_dict
+
+
+# Listado de eventos disponible en la web de la upm
+def events_json_scraper():
+    html = requests.get("https://etsisi.upm.es")
+    soup = BeautifulSoup(html.text, 'html.parser')
+    events_dict = {
+    }
+    for idx, row in enumerate(soup.find(id='block-views-calendario-block-2--2').findAll('a')):
+        events_dict[idx] = {}
+        events_dict[idx]["text"] = row.string
+        events_dict[idx]["a-link"] = '<a href="https://etsisi.upm.es' + row.get("href") + '">' + row.string + '</a>'
+        events_dict[idx]["link"] = 'https://etsisi.upm.es' + row.get("href")
+    return events_dict
+
+
+def avisos_json_scraper():
+    html = requests.get("https://etsisi.upm.es/alumnos/avisos")
+    soup = BeautifulSoup(html.text, 'html.parser')
+    avisos_dict = {}
+
+    for idx, row in enumerate(soup.find(id='main-content').findAll('a')):
+        if idx > 5:
+            continue
+        avisos_dict[idx] = {}
+        avisos_dict[idx]["text"] = row.string
+        avisos_dict[idx]["a-link"] = '<a href="https://etsisi.upm.es' + row.get("href") + '">' + row.string + '</a>'
+        avisos_dict[idx]["link"] = 'https://etsisi.upm.es' + row.get("href")
+
+    avisos_dict[6] = {}
+    avisos_dict[6]["text"] = "Más avisos..."
+    avisos_dict[6]["a-link"] = '<a href="https://etsisi.upm.es/alumnos/avisos">Más avisos...</a>'
+    avisos_dict[6]["link"] = 'https://etsisi.upm.es/alumnos/avisos'
+    return avisos_dict
+
+
+if __name__ == "__main__":
+    print(news_json_scraper())
+    print(events_json_scraper())
+    print(avisos_json_scraper())
