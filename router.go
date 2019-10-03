c015fe523a49ccd1a4e145df634bc6dc0fb368ee
Ricardo <rsanz@hardwarriors.com>
2019-10-03 19:00:57 +0200

    Gives menu2 command to a more descriptive name

--- router.go
+++ router.go
@@ -21,7 +21,7 @@ import (
 // Note that underscores (_) are forbidden for command names.
 func route(cmd *commander.CommandGroup, cfg config, callbacks *commander.CommandGroup) {
 	cmd.Command("/menu", menu.CafeTodayCmd)
-	cmd.Command("/menu2", menu.CafeTomorrowCmd)
+	cmd.Command("/menu_mañana", menu.CafeTomorrowCmd)
 	cmd.Command("/salas", salas.SalasCmd)
 	cmd.Command("/noticias", news.NewsCmd)
 	cmd.Command("/avisos", news.AvisosCmd)
